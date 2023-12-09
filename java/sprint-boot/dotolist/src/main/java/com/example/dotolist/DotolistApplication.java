580df4a4b3ea2ebbf0dd711f79efac1dd7bda188
Nuttapat Chakpech <nuttapat.swd@gmail.com>
2023-12-09 13:16:20 +0700

    [Feature] initial Spring boot project

---
+++ java/sprint-boot/dotolist/src/main/java/com/example/dotolist/DotolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.dotolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotolistApplication.class, args);
+	}
+
+}
---
+++ java/sprint-boot/dotolist/src/test/java/com/example/dotolist/DotolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dotolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
