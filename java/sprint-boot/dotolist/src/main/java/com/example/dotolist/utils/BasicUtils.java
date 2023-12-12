8796512a726d7d5858455e0a9ab050a4173028f4
Nuttapat Chakpech <nuttapat.swd@gmail.com>
2023-12-12 08:54:59 +0700

    [Feature] Implement BasicUtils - create getNullPropertyNames fucntion

---
+++ java/sprint-boot/dotolist/src/main/java/com/example/dotolist/utils/BasicUtils.java
@@ -0,0 +1,27 @@
+package com.example.dotolist.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class BasicUtils {
+
+    public static <T> String[] getNullPropertyNames(T source){
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        return emptyNames.toArray(new String[0]);
+
+    }
+
+}
